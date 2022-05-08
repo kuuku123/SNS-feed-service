30c75fea37bec26c59bf56a6ef42606591fa5f97
kuuku123 <dohyun0047@gmail.com>
2022-05-08 12:07:02 +0900

    docker-compose added

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://localhost:8300';
 // export const API_BASE_URL = '';
 export const JWT = 'jwt';
 
